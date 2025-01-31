2ac89403819b86e87e565d41184accec6b6dbd4b
savnkr <147591398+savnkr@users.noreply.github.com>
2025-01-31 11:01:47 +0000

    update

---
+++ assets/js/custom.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+    new Typed("#intro", {
+      strings: [
+        "Hi! I am Sawan Kumar, a PhD student at IIT Delhi.",
+      ],
+      typeSpeed: 50,
+      backSpeed: 30,
+      loop: true,
+    });
+  });
+  
